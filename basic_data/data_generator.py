042bfb8aa8efed54345163da422b7fd1643b8e26
Duo <luduo1999@126.com>
2019-12-06 18:36:21 -0800

    add

---
+++ basic_data/data_generator.py
@@ -0,0 +1,27 @@
+import random
+
+def main(num):
+    num = int(num)
+    file = open("basicData_num_" + str(num) + ".dzn" , 'w')
+
+    file.write("% number of games is " + str(num))
+    file.write("\n\n")
+    file.write("num = " + str(num) + ";")
+    file.write('\n')
+    file.write("cap = " + str(5) + ";")
+    file.write('\n')
+    file.write("refill = " + str(2) + ";")
+    file.write('\n')
+
+    fun = []
+    for i in range(num):
+        fun.append(random.randint(-20,20))
+    print(fun)
+    
+    file.write("fun = " + str(fun) + ";")
+    file.write('\n')
+    
+
+if __name__ == "__main__":
+    num = input("Input the number of games: ")
+    main(num)
